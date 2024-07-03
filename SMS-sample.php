3cc7873565a8477f11434562615e6278e5cf146c
SMSGATEWAYHUB <tech.support@stripl.in>
2024-07-03 16:13:53 +0530

    Add files via upload

---
+++ SMS-sample.php
@@ -0,0 +1,19 @@
+<?php
+$apikey = "YourAPIkey";
+$apisender = "TESTIN";
+$msg ="YOUR MESSAGE HERE";
+$num = 91XXXXXXXXX; // MULTIPLE NUMBER VARIABLES PUT HERE...!
+$ms = rawurlencode($msg); //This for encode your message content
+$url =
+'https://www.smsgatewayhub.com/api/mt/SendSMS?APIKey='.$apikey.'&senderid='.$apisender.
+'&channel=2&DCS=0&flashsms=0&number='.$num.'&text='.$ms.'&route=1';
+//echo $url;
+$ch=curl_init($url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch,CURLOPT_POST,1);
+curl_setopt($ch,CURLOPT_POSTFIELDS,"");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,2);
+$data = curl_exec($ch);
+echo '<br/><br/>';
+print($data); /* result of API call*/
+?>
